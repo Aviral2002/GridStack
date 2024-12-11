5b150c909aed721917e353e267443ffc2927cdac
Aviral Srivastava <aviralsrivastava121@gmail.com>
2024-12-11 14:44:15 +0530

    Prep for frontend deplyement

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,5 +13,8 @@ export default defineConfig({
   server: {
     port: process.env.PORT || 3000,
   },
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+  },
 })
 
